fe1cdc33c77d0fe9f85ca95480c12004a62062a5
Sebastian <1144105003@u.icesi.edu.co>
2024-11-01 22:45:23 -0500

    first commit

---
+++ src/main/java/com/mycompany/docker/mavenproject1/RestServiceApplication.java
@@ -0,0 +1,21 @@
+package com.mycompany.docker.mavenproject1;
+
+import java.util.Collections;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Clase para iniciar Spring
+@SpringBootApplication
+public class RestServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(RestServiceApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", getPort()));
+        app.run(args);
+    }
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 33025;
+    }
+}
